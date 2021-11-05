89f0918f75e9edd61661976d51c6116f3ad6e6d0
sergJess <serg.fantasy@yandex.ru>
2021-11-05 12:23:28 +0300

    feat: add task-2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,5 +1,26 @@
 const path = require('path');
 const fs = require('fs');
+const process = require('process');
+const stdinProcess = process.stdin;
+const stdoutProcess = process.stdout;
 const readline = require('readline');
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf8');
-console.log('Greeting press');
+const rl = readline.createInterface(stdinProcess, stdoutProcess);
+console.log('Greeting!');
+rl.on('line', (input) => {
+  if(input === 'exit')
+  {
+    console.log('Goodbye');
+    rl.close();
+  }
+  else
+  {
+    output.write(`${input}\n`);
+  }
+ 
+});
+rl.on('SIGINT',()=>{
+  console.log('Goodbye...');
+  rl.close();
+});
+
